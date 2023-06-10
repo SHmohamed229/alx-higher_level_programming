ff7dbc458b88e34c4e3e942989c195c5de96f10b
SHmohamed229 <SHmohamed229@gmail.com>
2023-06-10 09:16:45 -0700

    all task

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 8-multiple_returns.py
+
+
+def multiple_returns(sentence):
+    """for Returns the length of a string and its first char."""
+    if sentence == "":
+        return (0, None)
+    return (len(sentence), sentence[0])
