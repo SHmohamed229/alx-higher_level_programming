d43b921fb92dbbf0eff210b1121b108c697ae215
SHmohamed229 <SHmohamed229@gmail.com>
2023-08-15 04:30:02 -0700

    task adv

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,9 @@
+-- Moved or converts hbtn_0c_0 database to utf8
+-- table: This for first_table to utf8
+-- field: This for name to utf8
+
+ALTER DATABASE htbn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
