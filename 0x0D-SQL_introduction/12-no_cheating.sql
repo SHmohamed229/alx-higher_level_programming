225fc9d15feb61975feeec2055275db29d71be74
SHmohamed229 <SHmohamed229@gmail.com>
2023-08-15 04:24:51 -0700

    task 12 , 13 , 14 , 15 ,16

---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,5 @@
+--THis Query for  update score of Bob to 10
+UPDATE `second_table`
+SET
+`score` = 10
+WHERE `second_table` . `name` = 'Bob';
---
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -0,0 +1,3 @@
+-- This Query for Removes all scores below 5 from second_table
+DELETE FROM second_table
+WHERE score <= 5;
